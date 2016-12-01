6bc972f2a0364a391adcb8bf52393219845b3939
putanowr <R.Putanowicz@L5.pk.edu.pl>
2016-12-01 11:56:11 +0100

    Matrix assembly implemented as template function

--- src/assem.cpp
+++ src/assem.cpp
@@ -1,18 +1,10 @@
-#include <armadillo>
+#include "assem.h"
+
 #include <numeric>
+
 
 namespace armafem {
 
-using namespace arma;
-
-bool assem(const umat edof, mat &K, const mat &Ke, colvec &f, const colvec &fe) {
-  auto const &t = edof.cols(1,edof.n_cols); // skip the first column which is elment numbers
-  for (auto i = 0; i<edof.n_rows; ++i) {
-    const auto &tr = t.row(i);
-    K(tr, tr) += Ke;
-    f(tr) += fe;
-  }
-}
 
 } // namespace armafem
 
