24a6946e881a9ccef311a7f86f48fd5f88081fd0
putanowr <putanowr@L5.pk.edu.pl>
2016-12-02 19:58:22 +0100

    Impoved naming of variables. Added Doxygen documentation to solveq

--- src/solveq.cpp
+++ src/solveq.cpp
@@ -4,27 +4,25 @@ using namespace arma;
 
 namespace armafem {
 
-bool solveq(const mat &K, const colvec &f, const uvec& pdof, const colvec &dp,
+void solveq(const mat &K, const colvec &f, const uvec& fixId, const colvec &fixVal,
             colvec &d, colvec &Q) {
      auto nd = K.n_rows;
      uvec mask = ones<uvec>(nd);
-     mask.elem(pdof).zeros();
-     auto fdof = find(mask > 0);
+     mask.elem(fixId).zeros();
+     auto activeId = find(mask > 0);
 
      d=zeros(nd,1);
      Q=zeros(nd,1);
      
-     mat Kr = K.submat(fdof, fdof);
-     mat Kd = K.submat(fdof, pdof);
-     colvec fr = f.elem(fdof) - Kd*dp;
+     mat Kr = K.submat(activeId, activeId);
+     mat Kd = K.submat(activeId, fixId);
+     colvec fr = f.elem(activeId) - Kd*fixVal;
 
      colvec s = solve(Kr, fr);
 
-     d.elem(pdof)=dp;
-     d.elem(fdof)=s;
+     d.elem(fixId)=fixVal;
+     d.elem(activeId)=s;
      Q=K*d-f;
-  
-     return true;
 }
 
 } // namespace armafem
--- src/solveq.h
+++ src/solveq.h
@@ -3,6 +3,14 @@
 #include <armadillo>
 
 namespace armafem {
-bool solveq(const arma::mat &K, const arma::colvec &f, const arma::uvec& pdof, const arma::colvec &dp,
+//! Solve static FE-equations considering boundary conditions.
+//!
+//! \param[in] K       global stiffness matrix, dim(K) = nd x nd
+//! \praam[in] f       global load vector, dim(f) = nd x 1
+//! \param[in] fixId   indices of constrainded DOFs 
+//! \param[in] fixVal  constrainded DOF values 
+//! \param[out] d      solution including constrained values dim(d) = nd x 1
+//! \param[out] Q      reaction force vector dim(Q) = nd x 1
+void solveq(const arma::mat &K, const arma::colvec &f, const arma::uvec& fixId, const arma::colvec &fixVal,
             arma::colvec &d, arma::colvec &Q);
 } // namespace armafem
