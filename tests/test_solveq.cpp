93e191a44c4006d18225a90f95b5d13ed42386aa
putanowr <putanowr@L5.pk.edu.pl>
2016-12-02 20:23:36 +0100

    Added test for reaction forces output vector

--- tests/test_solveq.cpp
+++ tests/test_solveq.cpp
@@ -7,6 +7,12 @@
 using namespace arma;
 using namespace armafem;
 
+//  Build and solves system:
+//  |4 2 0 0|  |x0=1|     |0|
+//  |2 4 0 0|  |x1  |  =  |6|
+//  |0 0 4 2|  |x2  |     |6|
+//  |0 0 2 4|  |x3=1|     |0|
+//
 TEST_CASE("solveq test", "[assembly]")
 {
   auto Ke = mat{{4,2},{2,4}};
@@ -16,9 +22,12 @@ TEST_CASE("solveq test", "[assembly]")
 
   auto pdof = uvec{0,3};
   auto dp = colvec{1.0, 1.0};
-  auto f = colvec{0, 6,6,0};
+  auto f = colvec{0.0, 6.0, 6.0, 0.0};
   auto d = colvec{};
   auto Q = colvec{};
+  auto Qexpected = colvec{6.0, 0.0, 0.0, 6.0};
   armafem::solveq(K, f, pdof, dp, d, Q);
   CHECK(true == approx_equal(d, ones<colvec>(4), "absdiff", 1.0e-5));
+  CHECK(true == approx_equal(Q, Qexpected, "absdiff", 1.0e-5));
+
 }
